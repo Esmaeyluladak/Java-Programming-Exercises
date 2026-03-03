78fb3c83f321afb3d07ca751a554547eefb15054
Esma Eylül Adak <esmaeyluladak@gmail.com>
2026-03-04 00:54:21 +0300

    Update soru2.java

---
+++ soru2.java
@@ -0,0 +1,20 @@
+public class soru2 {
+    public static void main(String[] args){
+     int not = 81;
+     if(not>=90){
+        System.out.println("AA");
+      }
+     else if (not>=80) {
+         System.out.println("BB");
+      }
+     else if (not>=50) {
+         System.out.println("CC");
+      }
+     else if (not>=0) {
+         System.out.println("FF");
+      }
+     else {
+         System.out.println("Hatalı giriş.");
+       }
+    }
+}
