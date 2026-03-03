c52bf60ad526e1356eccbb7dfcbf0ffb8e92c5bc
Esma Eylül Adak <esmaeyluladak@gmail.com>
2026-03-04 00:53:19 +0300

    Update soru1.java

---
+++ soru1.java
@@ -0,0 +1,10 @@
+public class soru1 {
+    public static void main(String[] args){
+
+        double urunFiyati = 899.90;
+        String urunAdi = "parfüm";
+        boolean urunstoktaMi = true;
+        System.out.println(urunAdi + " ürününün fiyatı " + urunFiyati + " TL'dir" + " ve stok durumu:" + urunstoktaMi );
+    }
+
+}
