d6e54dcc3781025d999fa2486f3488d57cff3f64
Esma Eylül Adak <esmaeyluladak@gmail.com>
2026-03-04 00:55:39 +0300

    Update soru6.java

---
+++ soru6.java
@@ -0,0 +1,15 @@
+class Ogrenci {
+    String ad;
+    String soyad;
+    int ogrenciNo;
+}
+public class soru6 {
+    public static void main(String[] args) {
+        Ogrenci o1 = new Ogrenci();
+        o1.ad = "Ahmet";
+        o1.soyad = "Yılmaz";
+        o1.ogrenciNo = 101;
+
+        System.out.println(o1.ad +" "+ o1.soyad +" " + o1.ogrenciNo);
+    }
+}
