756b8a2ed2591ecb075dbf042e9232c3824a858a
Esma Eylül Adak <esmaeyluladak@gmail.com>
2026-03-04 00:47:42 +0300

    Update KalitimTest.java

---
+++ KalitimTest.java
@@ -0,0 +1,8 @@
+public class KalitimTest {
+    public static void main(String[] args) {
+
+        araba benimArabam = new araba("Mercedes", "C200", 4);
+        benimArabam.bilgileriGoster();
+    }
+
+}
